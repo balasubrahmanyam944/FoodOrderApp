8c2e18a20138f118dc124476d281bf33f7d7e68f
balasubrahmanyam944 <balasubrahmanyam944@gmail.com>
2025-06-18 23:41:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    allowedHosts: ['foodorder-backend-jif9.onrender.com'], // ✅ Your backend URL
+  },
+});
