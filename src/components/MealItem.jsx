c66b3071b9a3422eb3dd6e931ea3d161f0ef9c8d
balasubrahmanyam944 <balasubrahmanyam944@gmail.com>
2025-06-19 15:47:22 +0530

    Update MealItem.jsx

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,30 +1,46 @@
-import Button from "./UI/Button"
-import { useContext } from "react";
-import { currencyFormatter } from "../util/formatting"
+import { useState, useContext } from "react";
+import Button from "./UI/Button";
+import { currencyFormatter } from "../util/formatting";
 import CartContext from "./store/CartContext";
 
 export default function MealItem({ meal }) {
-    const cartCtx = useContext(CartContext);
-    function handleAddMealToCart() {
-        console.log("Adding meal to cart:", meal);
-        
-        cartCtx.addItem(meal);
-    }
-    return (
-      <li className="meal-item">
-        <article>
-          <img src={`https://foodorder-backend-jif9.onrender.com/${meal.image}`} alt={meal.name} />
-          <div>
-            <h3>{meal.name}</h3>
-            <p className="meal-item-price">
-              {currencyFormatter.format(meal.price)}
-            </p>
-            <p className="meal-item-description">{meal.description}</p>
-          </div>
-          <p className="meal-item-actions">
-            <Button onClick={handleAddMealToCart}>Add to Cart</Button>
-          </p>
-        </article>
-      </li>
-    );
+  const [showModal, setShowModal] = useState(false); // state for modal visibility
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    console.log("Adding meal to cart:", meal);
+    
+    cartCtx.addItem(meal);
+
+    // Show the modal
+    setShowModal(true);
+
+    // Hide the modal after 1 second
+    setTimeout(() => {
+      setShowModal(false);
+    }, 1000);
+  }
+
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3001/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
+      </article>
+
+      {/* Modal for "Added to Cart" */}
+      {showModal && (
+        <div className="modal1">
+          <p>Added to Cart!</p>
+        </div>
+      )}
+    </li>
+  );
 }
