e78f4b9ecff78c86a7848b3a060f6afd07061274
balasubrahmanyam944 <balasubrahmanyam944@gmail.com>
2025-06-19 15:52:40 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
       <header id="main-header">
         <div id="title">
           <img src={logoImg} alt="A restaurant" />
-          <h1>Vikash Food Court</h1>
+          <h1>Balu Food Court</h1>
         </div>
         <nav>
           <Button textOnly onClick={handleShowCart}>
